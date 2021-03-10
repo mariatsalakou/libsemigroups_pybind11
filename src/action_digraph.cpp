276cc6949cbc9ee6b7586c0ac41f7147c0699090
MariaT39 <mariatsalakou@gmail.com>
2021-03-10 17:56:27 +0200

    Added action digraph

--- src/action_digraph.cpp
+++ src/action_digraph.cpp
@@ -1,7 +1,7 @@
 //
 // libsemigroups - C++ library for semigroups and monoids
-// Copyright (C) 2021 James D. Mitchell + Maria Tsalakou
-//
+// Copyright (C) 2021 James D. Mitchell 
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
